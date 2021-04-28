45d6811a60ae877acc8b93307dfb3551dcea0ee3
devjinjin <devjinjin@gmail.com>
2021-04-28 20:24:15 +0900

    팝업출력

--- Album/ViewController.swift
+++ Album/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func TestButton(_ sender: UIButton) {
+        
+        //팝업
+        let alert = UIAlertController(title: "Hello", message: "Wow", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        //팝업
+    }
+    
 }
 
